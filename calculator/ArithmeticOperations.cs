8a6cb5f759310f461be1a363bc0efee6ca671a5a
Anjana <74anjana74@gmail.com>
2025-08-08 11:32:49 +0530

    Add project files.

---
+++ calculator/ArithmeticOperations.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculator
+{
+    public class ArithmeticOperations : IArithematicOperations
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+        public int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+        public int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+        public int Divide(int a, int b)
+        {
+            if (b == 0)
+            {
+                throw new DivideByZeroException("Division by zero is not allowed.");
+            }
+            return a / b;
+        }
+    }
+}
---
+++ calculator/MainWindow.xaml.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace calculator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string eqn = string.Empty;
+        IArithematicOperations operations;
+        Dictionary<char, Func<int, int, int>> operationMap;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            operations = new ArithmeticOperations();
+            InitializeOpMapping();
+        }
+
+        private void InitializeOpMapping()
+        {
+            operationMap = new Dictionary<char, Func<int, int, int>>()
+            {
+                { '+', operations.Add },
+                { '-', operations.Subtract },
+                { '*', operations.Multiply },
+                { '/', operations.Divide },
+            };
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Button btn = (Button)sender;
+            eqn += btn.Content.ToString();
+            TextRes.Text = eqn;
+        }
+
+        private void DivideButton_Click(object sender, RoutedEventArgs e)
+        {
+            eqn += '/';
+            TextRes.Text = eqn;
+        }
+
+        private void MultiplyButton_Click(object sender, RoutedEventArgs e)
+        {
+            eqn += '*';
+            TextRes.Text = eqn;
+        }
+
+        private void SubButton_Click(object sender, RoutedEventArgs e)
+        {
+            eqn += '-';
+            TextRes.Text = eqn;
+        }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            eqn += '+';
+            TextRes.Text = eqn;
+        }
+
+        private void EqualsTo_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                int result = EvaluateEquation(eqn);
+                TextRes.Text = eqn + "=" + result.ToString();
+                eqn = result.ToString(); 
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void Clear_Click(object sender, RoutedEventArgs e)
+        {
+            eqn = String.Empty;
+            TextRes.Clear();
+        }
+        private int EvaluateEquation(string eqn)
+        {
+            string[] parts = eqn.Split(new char[] { '+', '-', '*', '/' });
+            int first = Int32.Parse(parts[0]);
+            int second = Int32.Parse(parts[1]);
+            char op = eqn[parts[0].Length];
+            if (op == '/' && second == 0)
+            {
+                throw new DivideByZeroException("Division by zero not possible.");
+            }
+
+            if (operationMap.TryGetValue(op, out var operation))
+            {
+                return operation(first, second);
+            }
+            else
+            {
+                throw new InvalidOperationException("Invalid operation.");
+            }
+        }
+    }
+}
