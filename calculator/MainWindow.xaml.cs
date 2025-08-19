dfe9e22bbc6050dda1308baa94cafcbeeb93f2b3
Anjana <74anjana74@gmail.com>
2025-08-19 14:47:38 +0530

    updated variable names according to naming conventions

--- calculator/MainWindow.xaml.cs
+++ calculator/MainWindow.xaml.cs
@@ -7,46 +7,45 @@ namespace calculator
 {
     public partial class MainWindow : Window
     {
-        private string eqn = string.Empty;
+        private string Equation = string.Empty;
 
-        private readonly IArithematicOperations<int> intOps;
-        private readonly IArithematicOperations<decimal> decOps;
+        private Dictionary<char, Func<int, int, int>> IntegerOperationMap;
+        private Dictionary<char, Func<decimal, decimal, decimal>> DecimalOperationMap;
 
-        private Dictionary<char, Func<int, int, int>> intOperationMap;
-        private Dictionary<char, Func<decimal, decimal, decimal>> decOperationMap;
-
+        private readonly IArithematicOperations<int> IntegerOperations;
+        private readonly IArithematicOperations<decimal> DecimalOperations;
         public MainWindow()
         {
             InitializeComponent();
-            intOps = new IntArithmeticOperations();
-            decOps = new DecimalArithmeticOperations();
+            IntegerOperations = new IntArithmeticOperations();
+            DecimalOperations = new DecimalArithmeticOperations();
             InitializeOpMapping();
         }
 
         private void InitializeOpMapping()
         {
-            intOperationMap = new Dictionary<char, Func<int, int, int>>()
+            IntegerOperationMap = new Dictionary<char, Func<int, int, int>>()
             {
-                { '+', intOps.Add },
-                { '-', intOps.Subtract },
-                { '*', intOps.Multiply },
-                { '/', intOps.Divide },
+                { '+', IntegerOperations.Add },
+                { '-', IntegerOperations.Subtract },
+                { '*', IntegerOperations.Multiply },
+                { '/', IntegerOperations.Divide },
             };
 
-            decOperationMap = new Dictionary<char, Func<decimal, decimal, decimal>>()
+            DecimalOperationMap = new Dictionary<char, Func<decimal, decimal, decimal>>()
             {
-                { '+', decOps.Add },
-                { '-', decOps.Subtract },
-                { '*', decOps.Multiply },
-                { '/', decOps.Divide },
+                { '+', DecimalOperations.Add },
+                { '-', DecimalOperations.Subtract },
+                { '*', DecimalOperations.Multiply },
+                { '/', DecimalOperations.Divide },
             };
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            Button btn = (Button)sender;
-            eqn += btn.Content.ToString();
-            TextRes.Text = eqn;
+            Button Button = (Button)sender;
+            Equation += Button.Content.ToString();
+            TextRes.Text = Equation;
         }
 
         private void DivideButton_Click(object sender, RoutedEventArgs e) => AppendOperator('/');
@@ -55,61 +54,56 @@ namespace calculator
         private void AddButton_Click(object sender, RoutedEventArgs e) => AppendOperator('+');
         private void DecimalButton_Click(object sender, RoutedEventArgs e) => AppendOperator('.');
 
-        private void AppendOperator(char op)
+        private void AppendOperator(char Operator)
         {
-            eqn += op;
-            TextRes.Text = eqn;
+            Equation += Operator;
+            TextRes.Text = Equation;
         }
 
         private void EqualsTo_Click(object sender, RoutedEventArgs e)
         {
             try
             {
-                string result = EvaluateEquation(eqn);
-                TextRes.Text = eqn + "=" + result;
-                eqn = result;
+                string result = EvaluateEquation(Equation);
+                TextRes.Text = Equation + "=" + result;
+                Equation = result;
             }
             catch (Exception ex)
             {
-                MessageBox.Show(ex.Message);
+                 TextRes.Text = ex.Message;
             }
         }
 
         private void Clear_Click(object sender, RoutedEventArgs e)
         {
-            eqn = String.Empty;
+            Equation = String.Empty;
             TextRes.Clear();
         }
 
-        private string EvaluateEquation(string eqn)
+        private string EvaluateEquation(string Equation)
         {
-            string[] parts = eqn.Split(new char[] { '+', '-', '*', '/' });
+            string[] parts = Equation.Split(new char[] { '+', '-', '*', '/' });
 
-            bool isDecimal = eqn.Contains(".");
+            bool isDecimal = Equation.Contains(".");
 
-            char op = eqn[parts[0].Length];
+            char Operator = Equation[parts[0].Length];
 
             if (isDecimal)
             {
-                decimal first = Decimal.Parse(parts[0]);
-                decimal second = Decimal.Parse(parts[1]);
+                decimal FirstOperand = Decimal.Parse(parts[0]);
+                decimal SecondOperand = Decimal.Parse(parts[1]);
 
-                if (op == '/' && second == 0)
-                    throw new DivideByZeroException("Division by zero not possible.");
 
-                if (decOperationMap.TryGetValue(op, out var operation))
-                    return operation(first, second).ToString();
+                if (DecimalOperationMap.TryGetValue(Operator, out var operation))
+                    return operation(FirstOperand, SecondOperand).ToString();
             }
             else
             {
-                int first = Int32.Parse(parts[0]);
-                int second = Int32.Parse(parts[1]);
+                int FirstOperand = Int32.Parse(parts[0]);
+                int SecondOperand = Int32.Parse(parts[1]);
 
-                if (op == '/' && second == 0)
-                    throw new DivideByZeroException("Division by zero not possible.");
-
-                if (intOperationMap.TryGetValue(op, out var operation))
-                    return operation(first, second).ToString();
+                if (IntegerOperationMap.TryGetValue(Operator, out var operation))
+                    return operation(FirstOperand, SecondOperand).ToString();
             }
 
             throw new InvalidOperationException("Invalid operation.");
